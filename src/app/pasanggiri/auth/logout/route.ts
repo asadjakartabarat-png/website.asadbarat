3694f523c09cc6b98b14126dd941c9dab88adaef
asadjakartabarat-png <asad.jakartabarat@gmail.com>
2026-02-25 15:23:48 +0700

    Delete src/app/pasanggiri directory

--- src/app/pasanggiri/auth/logout/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function POST() {
-  const response = NextResponse.json({ success: true });
-  response.cookies.set('pasanggiri_session', '', { maxAge: 0, path: '/' });
-  return response;
-}
