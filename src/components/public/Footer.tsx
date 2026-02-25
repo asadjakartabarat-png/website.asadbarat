c5a5289e3e377089460bc319257ab95cfbdb3e39
asadjakartabarat-png <asad.jakartabarat@gmail.com>
2026-02-25 11:23:50 +0700

    Update Footer.tsx

--- src/components/public/Footer.tsx
+++ src/components/public/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
       <div className="container mx-auto px-4 py-8">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           <div>
-            <h3 className="text-xl font-bold text-primary-500 mb-4">Portal Kegiatan Persinas Asad Kota Jakarta Barat</h3>
+            <h3 className="text-xl font-bold text-primary-500 mb-4">Portal Kegiatan Persinas Asad<br />Kota Jakarta Barat</h3>
             <p className="text-gray-300">
               Portal berita kegiatan persinas asad kota jakarta barat terkini
             </p>
